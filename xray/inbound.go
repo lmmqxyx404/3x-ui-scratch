b5c96171010302e161abf40ee4c596efea94e276
lmmqxyx <1368470804@qq.com>
2025-01-05 15:58:50 +0000

    feat: Start succeed tag52

--- xray/inbound.go
+++ xray/inbound.go
@@ -1,46 +1,47 @@
 package xray
 
-// todo: change the following Equals
-type InboundConfig struct {
-	Tag  string `json:"tag"`
-	Port int    `json:"port"`
-	/* Listen         json_util.RawMessage `json:"listen"` // listen cannot be an empty string
+import (
+	"bytes"
 
+	"x-ui-scratch/util/json_util"
+)
+
+// note: 这个配置与 config.json 的生成有关联
+type InboundConfig struct {
+	Listen         json_util.RawMessage `json:"listen"` // listen cannot be an empty string
+	Port           int                  `json:"port"`
 	Protocol       string               `json:"protocol"`
 	Settings       json_util.RawMessage `json:"settings"`
 	StreamSettings json_util.RawMessage `json:"streamSettings"`
-
+	Tag            string               `json:"tag"`
 	Sniffing       json_util.RawMessage `json:"sniffing"`
-	Allocate       json_util.RawMessage `json:"allocate"` */
+	Allocate       json_util.RawMessage `json:"allocate"`
 }
 
-// todo： complete Equals
 func (c *InboundConfig) Equals(other *InboundConfig) bool {
-	if c.Tag != other.Tag {
+	if !bytes.Equal(c.Listen, other.Listen) {
 		return false
 	}
 	if c.Port != other.Port {
 		return false
 	}
-	/* 	if !bytes.Equal(c.Listen, other.Listen) {
-	   		return false
-	   	}
-
-	   	if c.Protocol != other.Protocol {
-	   		return false
-	   	}
-	   	if !bytes.Equal(c.Settings, other.Settings) {
-	   		return false
-	   	}
-	   	if !bytes.Equal(c.StreamSettings, other.StreamSettings) {
-	   		return false
-	   	}
-
-	   	if !bytes.Equal(c.Sniffing, other.Sniffing) {
-	   		return false
-	   	}
-	   	if !bytes.Equal(c.Allocate, other.Allocate) {
-	   		return false
-	   	} */
+	if c.Protocol != other.Protocol {
+		return false
+	}
+	if !bytes.Equal(c.Settings, other.Settings) {
+		return false
+	}
+	if !bytes.Equal(c.StreamSettings, other.StreamSettings) {
+		return false
+	}
+	if c.Tag != other.Tag {
+		return false
+	}
+	if !bytes.Equal(c.Sniffing, other.Sniffing) {
+		return false
+	}
+	if !bytes.Equal(c.Allocate, other.Allocate) {
+		return false
+	}
 	return true
 }
