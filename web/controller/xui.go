fd37e0c25f56a6ec79554eb0b08c9c4e1d365616
lmmqxyx <1368470804@qq.com>
2025-01-13 15:46:23 +0000

    feat: add a new route inbounds tag1

--- web/controller/xui.go
+++ web/controller/xui.go
@@ -20,9 +20,16 @@ func (a *XUIController) initRouter(g *gin.RouterGroup) {
 	g = g.Group("/panel")
 	g.Use(a.checkLogin)
 	g.GET("/", a.index)
+	// note: vital router
+	g.GET("/inbounds", a.inbounds)
 	logger.Info("TODO: add init router")
+
 }
 
 func (a *XUIController) index(c *gin.Context) {
 	html(c, "index.html", "pages.index.title", nil)
+}
+
+func (a *XUIController) inbounds(c *gin.Context) {
+	html(c, "inbounds.html", "pages.inbounds.title", nil)
 }
